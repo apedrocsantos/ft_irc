c2cb228408f2265cc5137699d3c2cb23eae2db83
JustShush <marquesdinis.p@gmail.com>
2024-07-27 19:02:54 +0100

    added main header of the project

---
+++ inc/main.hpp
@@ -0,0 +1,14 @@
+#ifndef MAIN_HPP
+# define MAIN_HPP
+
+# include "header.hpp"
+
+# include <iostream>
+#include <unistd.h>
+#include <string>
+#include <cstring>
+#include <csignal>
+
+# define PORT 6667
+
+#endif
