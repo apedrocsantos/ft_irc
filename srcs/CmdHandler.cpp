14e6ce17006f71015b2af6e3b4837efbc74cd704
anda-cun <anda-cun@student.42lisboa.com>
2024-08-30 19:32:19 +0100

    minor stuff on main

--- srcs/CmdHandler.cpp
+++ srcs/CmdHandler.cpp
@@ -114,13 +114,14 @@ void CmdHandler::user(Command *cmd, Client *client, Server *server) {
 }
 
 void CmdHandler::pass(Command *cmd, Client *client, Server *server) {
+	(void) client;
 	std::string params = cmd->get_params();
 	std::stringstream ss(params);
 	std::string pass;
 
 	ss >> pass;
 
-	if (server->get_pwd() != pass) return std::cout << "some error idk, maybe ERR_PassWdMisMatch" << std::endl;
+	if (server->get_pwd() != pass) std::cout << "some error idk, maybe ERR_PassWdMisMatch" << std::endl;
 
 	// continue
 	// send pass success
