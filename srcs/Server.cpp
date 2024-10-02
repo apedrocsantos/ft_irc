0d163d32c74ac8b0892131ef6a92be7846dbc5d7
anda-cun <anda-cun@student.42lisboa.com>
2024-10-02 21:40:47 +0100

    some todos do finish

--- srcs/Server.cpp
+++ srcs/Server.cpp
@@ -1,43 +1,44 @@
 #include "../inc/main.hpp"
+#include <sys/socket.h>
 
 Server::Server(char *port, std::string pwd) : _name("ircserv"), command("", this)
 {
     int optval = 1;
     this->port = port;
     this->pwd = pwd;
-    std::memset(&hints, 0, sizeof hints);
-    this->hints.ai_family = PF_INET;
-    this->hints.ai_socktype = SOCK_STREAM;
-    this->hints.ai_flags = AI_PASSIVE;
+    std::memset(&hints, 0, sizeof hints); // init hints structure
+    this->hints.ai_family = PF_UNSPEC; // address family (IPv4 or IPv6)
+    this->hints.ai_socktype = SOCK_STREAM; // TCP protocol
+    this->hints.ai_flags = AI_PASSIVE; // Flag that indicates the socket address is intended to be used in a call to bind
 
-    if (getaddrinfo(NULL, port, &hints, &this->res))
+    if (getaddrinfo(NULL, port, &hints, &this->res)) // get address structure
     {
         freeaddrinfo(this->res);
         throw(std::runtime_error("getaddrinfo"));
     }
-    this->socketId = socket(this->res->ai_family, this->res->ai_socktype, this->res->ai_protocol);
+    this->socketId = socket(this->res->ai_family, this->res->ai_socktype, this->res->ai_protocol); // create socket
     if (this->socketId == -1)
     {
         freeaddrinfo(this->res);   
         throw(std::runtime_error("Can't create socket!"));
     }
-    if (fcntl(this->socketId, F_SETFL, O_NONBLOCK))
+    if (fcntl(this->socketId, F_SETFL, O_NONBLOCK)) // set fd to non-blocking mode - read and write operations return immediately if they cannot be completed
     {
         freeaddrinfo(this->res);
         throw(std::runtime_error("Error setting fcntl options"));
     }
-    if (setsockopt(socketId, SOL_SOCKET, SO_REUSEADDR, &optval, sizeof(optval)) < 0)
+    if (setsockopt(socketId, SOL_SOCKET, SO_REUSEADDR, &optval, sizeof(optval)) < 0) // set socket options to reuse port
     {
         freeaddrinfo(this->res);
         throw(std::runtime_error("Error setting socket options"));
     }
-    if(bind(this->socketId, this->res->ai_addr, this->res->ai_addrlen))
+    if(bind(this->socketId, this->res->ai_addr, this->res->ai_addrlen)) // bind socket to address and port
     {
         freeaddrinfo(this->res);
         throw(std::runtime_error("Can't bind to IP/port"));
     }
-    pollfd serverfd = {this->socketId, POLLIN, 0};
-            this->pollfds.push_back(serverfd);
+    pollfd serverfd = {this->socketId, POLLIN, 0}; // create a pollfd structure for the server
+    this->pollfds.push_back(serverfd);
     freeaddrinfo(this->res);
     if(listen(this->socketId, SOMAXCONN))
     {
@@ -47,15 +48,27 @@ Server::Server(char *port, std::string pwd) : _name("ircserv"), command("", this
     std::cout << "Setup ok\n";
     std::cout << "Server running on port " << this->port << std::endl;
 }
-
+//TODO: delete hanging msgs
 Server::~Server() {
-    for (it_pollfd = this->pollfds.begin(); it_pollfd != this->pollfds.end(); it_pollfd++)
+	std::cout << "Closing server\n";
+	for (std::map<int, char *>::iterator it = in_buf.begin(); it != in_buf.end(); it++) // delete hanging messages
+	{
+		std::map<int, char *>::iterator next = it;
+		if (++next == in_buf.end())
+			break;
+		delete[] it->second;
+        in_buf.erase(it);
+		it = next;
+	}
+    for (it_pollfd = this->pollfds.begin(); it_pollfd != this->pollfds.end(); it_pollfd++) // close all fds
         close(it_pollfd->fd);
     this->pollfds.clear();
-    for (it_map = this->client_list.begin(); it_map != this->client_list.end(); it_map++)
+    for (it_map = this->client_list.begin(); it_map != this->client_list.end(); it_map++) // delete all clients
         delete (it_map->second);
-    for (it_channel_list = this->channel_list.begin(); it_channel_list != this->channel_list.end(); it_channel_list++)
+    for (it_channel_list = this->channel_list.begin(); it_channel_list != this->channel_list.end(); it_channel_list++) // delete all channels
+	{
         delete(it_channel_list->second);
+	}
 }
 
 void Server::start()
@@ -64,20 +77,15 @@ void Server::start()
 
     if (!this->pollfds.size())
         return;
-    poll_count = poll(this->pollfds.data(), this->pollfds.size(), -1);
+    poll_count = poll(this->pollfds.data(), this->pollfds.size(), -1); // poll fds for messages
     if (poll_count == -1 && !stop)
         throw(std::runtime_error("Poll error"));
     if (poll_count != 0)
     {
-        for (it_pollfd = this->pollfds.begin(); it_pollfd != this->pollfds.end(); it_pollfd++)
+        for (it_pollfd = this->pollfds.begin(); it_pollfd != this->pollfds.end(); it_pollfd++) // iterate pollfd
         {
-            // client connecting
-            if (it_pollfd->fd == this->pollfds.begin()->fd && (it_pollfd->revents & POLLIN))
-            {
+            if (it_pollfd->fd == this->pollfds.begin()->fd && (it_pollfd->revents & POLLIN)) // if server getting request
                 add_client();
-                break;
-            }
-            // message received
             else if (it_pollfd->revents & POLLIN)
                 receive_msg();
 			else if (it_pollfd->revents & POLLOUT)
@@ -90,19 +98,21 @@ void Server::add_client()
 {
     struct sockaddr_in addr;
     socklen_t addr_size = sizeof(addr);
-    // char *hostname;
 
     std::cout << "Client asking to be accepted\n";
     this->new_fd = accept(this->socketId, (struct sockaddr *)&addr, &addr_size);
-    if (this->new_fd < 0)
-        std::cerr << "Unable to establish connection to client.\n";
+	if (this->new_fd < 0)
+	{
+		std::cerr << "Unable to establish connection to client.\n";
+		return;
+	}
     try
     {
-        pollfd clientfd = {this->new_fd, POLLIN | POLLOUT, 0};
+        pollfd clientfd = {this->new_fd, POLLIN | POLLOUT, 0}; // create pollfd structure for the client for reading and writing
         this->pollfds.push_back(clientfd);
-        char *hostname = inet_ntoa(addr.sin_addr);
-        client_list.insert(std::make_pair(this->new_fd, new Client(this->new_fd, std::string(hostname))));
-		// out_buf.insert(std::make_pair(this->socketId, ""));
+		char *hostname = inet_ntoa(addr.sin_addr);
+        client_list.insert(std::make_pair(this->new_fd, new Client(this->new_fd, hostname)));
+		this->it_pollfd = this->pollfds.begin();
         std::cout << "connected users: " << get_nb_connected_users() << std::endl;
     }
     catch(const std::exception& e)
@@ -148,22 +158,21 @@ void Server::remove_channel(std::string name)
     }
 }
 
-// Corrigir se msg começar com ctrl-d ou for interrompida por vários ctrl-d seguidos
 void Server::receive_msg()
 {
     int msg_size;
     std::string str;
     int index;
 
-    if (in_buf.find(it_pollfd->fd) == in_buf.end())
+    if (in_buf.find(it_pollfd->fd) == in_buf.end()) // if no buf created, create it nad set all chars to \0
     {
         in_buf[it_pollfd->fd] = new char[MESSAGE_BUFFER_SIZE];
         std::memset(in_buf[it_pollfd->fd], 0, MESSAGE_BUFFER_SIZE);
     }
-    str = in_buf[it_pollfd->fd];
+    str = in_buf[it_pollfd->fd]; // if in_buf exists, set str to it
     index = str.size();
-    msg_size = recv(it_pollfd->fd, in_buf.at(it_pollfd->fd) + index, MESSAGE_BUFFER_SIZE - index, 0);
-    if (msg_size <= 0)
+    msg_size = recv(it_pollfd->fd, in_buf.at(it_pollfd->fd) + index, MESSAGE_BUFFER_SIZE - index - 1, 0); // if in_buf exists, add to it at index position
+    if (msg_size <= 0) // client disconnected
     {
         if (client_list.find(it_pollfd->fd) == client_list.end())
             return ;
@@ -171,11 +180,11 @@ void Server::receive_msg()
         return ;
     }
     str = in_buf[it_pollfd->fd];
-    if (msg_size && (int) str.find('\n') != -1)
-    {
-        std::string str(in_buf.at(it_pollfd->fd));
+	for (int i = 0; i < (int)str.length(); i++)
+		std::cout << (int) str[i] << " ";
+	std::cout << std::endl;
+    if (msg_size && (int)str.find("\r\n") != -1) // if there's a message and a \n, execute command
         command = Command(str, this);
-    }
 }
 
 void Server::send_msg()
